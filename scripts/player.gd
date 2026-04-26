11d401c7b97e6c4e57ff15cdd9d9a9753d3b35e1
João Becker <94809555+Beckerziin@users.noreply.github.com>
2026-04-26 18:08:25 -0300

    v.0.3
    + Câmera segue o player
    + Mecanica de morte do zumbi
    + Base da criação do level1
    + Mecanica de aumento da range do zumbi apos te ver uma vez
    + Atualização nas animações do zumbi
    + Refatoração de algumas cenas do jogo

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 250.0
+const SPEED = 200.0
 
 var lastDirection: Vector2 = Vector2.RIGHT
 var isAttacking: bool = false
--- scripts/zombie.gd
+++ scripts/zombie.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
-const SPEED: int = 80
-const knockbackForce: int = 30
+const SPEED: int = 40
+const knockbackForce: int = 10
 
+var is_alive: bool = true
 var target = null
 var isAttacking = false
 var lastDirection: Vector2 = Vector2.DOWN
@@ -10,12 +11,15 @@ var health: int=100
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var take_damage_sound: AudioStreamPlayer2D = $takeDamage
+@onready var death_sound: AudioStreamPlayer2D = $deathSound
+@onready var health_bar: Node2D = $HealthBar
+@onready var zombieViewRadius: CollisionShape2D = $zombieSigth/CollisionShape2D
 
 
 func _physics_process(delta: float) -> void:
-	if target:
+	if is_alive and target:
 		playerTracking(delta)
-
+		
 	process_animation()
 	
 func playerTracking(delta: float) -> void:
@@ -30,25 +34,40 @@ func playerTracking(delta: float) -> void:
 
 func receiveDamage(damageReceived: int, attackerPosition: Vector2) -> void:
 	health -= damageReceived
-	take_damage_sound.play()
-	
-	#knockback
-	var knockbackDirection = (position - attackerPosition).normalized()
-	var targetPosition = position + knockbackDirection * knockbackForce
-	
-	var tween = create_tween()
-	tween.set_ease(Tween.EASE_OUT)
-	tween.set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(self, "position", targetPosition, 0.5)
-
-func process_animation() -> void:
-	if isAttacking == true:
-		return
-	if target != null:
-		play_animation("walk", lastDirection)
+	health_bar.update_health(health)
+	if health <=0:
+		_die()
 	else:
-		play_animation("idle", lastDirection)
+		take_damage_sound.play()
 		
+		#knockback
+		var knockbackDirection = (position - attackerPosition).normalized()
+		var targetPosition = position + knockbackDirection * knockbackForce
+		
+		var tween = create_tween()
+		tween.set_ease(Tween.EASE_OUT)
+		tween.set_trans(Tween.TRANS_CUBIC)
+		tween.tween_property(self, "position", targetPosition, 0.5)
+	
+	
+func _die() -> void:
+	if is_alive:
+		play_animation("death", lastDirection)
+		death_sound.play()
+	is_alive = false
+	
+	## Desaibilitar colisão
+	$zombieSigth/CollisionShape2D.set_deferred("disabled", true)
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func process_animation() -> void:
+	if is_alive:
+		if isAttacking == true:
+			return
+		if target != null:
+			play_animation("walk", lastDirection)
+		else:
+			play_animation("idle", lastDirection)
 
 func play_animation(prefix: String, dir: Vector2) -> void:
 	if dir.x > 0:
@@ -63,6 +82,7 @@ func play_animation(prefix: String, dir: Vector2) -> void:
 func _on_zombie_sigth_body_entered(body: Node2D) -> void:
 	if body.name == 'player':
 		target = body
+		zombieViewRadius.shape.set("radius", 144)
 
 
 func _on_zombie_sigth_body_exited(body: Node2D) -> void:
