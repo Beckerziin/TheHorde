0a2b8d11f90ecf48833e0bdaa4a3902fd73e842c
Ian-Siveris <imlsiveris@minha.fag.edu.br>
2026-04-27 00:48:59 -0300

    + HSlider em settings
    v 0.5

--- scenes/menu-inicial/settings_cena.gd
+++ scenes/menu-inicial/settings_cena.gd
@@ -1,15 +1,24 @@
 extends Control
 
+var bus_index: int
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# Prepara o barramento de áudio Master
+	bus_index = AudioServer.get_bus_index("Master")
+	
+	# Deixa a bolinha do slider na posição certa baseada no volume atual
+	# Atenção: Certifique-se de que o nome do nó na aba Cena é exatamente "HSlider"
+	$HSlider.value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 
 func _on_sair_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu-inicial/main_menu.tscn")
+
+
+# --- NOVA FUNÇÃO DO VOLUME ---
+func _on_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
